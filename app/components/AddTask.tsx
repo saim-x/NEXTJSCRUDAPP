c275680f5a4c60e3769e27d18d0ea828408b5b2c
saim-x <x_aci@hotmail.com>
2024-08-07 17:45:18 +0500

    UPDATE AND DELETE COMPLETED

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -5,6 +5,7 @@ import { FaPlusCircle } from "react-icons/fa";
 import Modal from "./Modal";
 import { addTodo } from "@/api";
 import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from 'uuid';
 
 const AddTask = () => {
     const router = useRouter();
@@ -15,7 +16,7 @@ const AddTask = () => {
     const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
         e.preventDefault();
         await addTodo({
-            id: Math.random().toString(),
+            id: uuidv4(),
             text: newTaskValue
         })
         setNewTaskValue("");
