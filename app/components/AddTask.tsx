a76379da6172f8708cbf8949ea253634341db35b
saim-x <x_aci@hotmail.com>
2024-08-07 17:14:13 +0500

    create and read done

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -3,22 +3,29 @@
 import React, { FormEventHandler, useState } from "react";
 import { FaPlusCircle } from "react-icons/fa";
 import Modal from "./Modal";
+import { addTodo } from "@/api";
+import { useRouter } from "next/navigation";
 
 const AddTask = () => {
+    const router = useRouter();
 
 
     const [modalOpen, setModalOpen] = React.useState<boolean>(false);
     const [newTaskValue, setNewTaskValue] = useState<string>("");
-    const handleSubmitNewTodo:FormEventHandler<HTMLFormElement> = (e) => {
+    const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
         e.preventDefault();
-        console.log("new task value", newTaskValue);
+        await addTodo({
+            id: Math.random().toString(),
+            text: newTaskValue
+        })
         setNewTaskValue("");
         setModalOpen(false);
+        router.refresh();
     }
     return (
         <div>
             <button onClick={
-                ()=>setModalOpen(true)
+                () => setModalOpen(true)
             } className="btn btn-primary w-full">
                 Add new task
                 <FaPlusCircle className="ml-2" size={16} />
@@ -30,10 +37,10 @@ const AddTask = () => {
                         Add new task
                     </h3>
                     <div className="modal-action">
-                    <input value={newTaskValue} onChange={(e) => setNewTaskValue(e.target.value)} type="text" placeholder="Type here" className="input input-bordered w-full" />
-                    <button className="btn" type="submit">
-                        Submit
-                    </button>
+                        <input value={newTaskValue} onChange={(e) => setNewTaskValue(e.target.value)} type="text" placeholder="Type here" className="input input-bordered w-full" />
+                        <button className="btn" type="submit">
+                            Submit
+                        </button>
                     </div>
                 </form>
             </Modal>
